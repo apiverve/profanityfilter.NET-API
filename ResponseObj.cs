448ee16af695722c56efdafddc1d689bc25c7235
APIVerve Team <hello@apiverve.com>
2024-05-14 15:37:46 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,38 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class data
+{
+    [JsonProperty("isProfane")]
+    public bool isProfane { get; set; }
+
+    [JsonProperty("filteredText")]
+    public string filteredText { get; set; }
+
+    [JsonProperty("mask")]
+    public string mask { get; set; }
+
+    [JsonProperty("trimmed")]
+    public bool trimmed { get; set; }
+
+    [JsonProperty("profaneWords")]
+    public string[] profaneWords { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
