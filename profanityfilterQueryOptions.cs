c1557b651412b2b511438e9948bce8c32aed6f92
APIVerve Team <hello@apiverve.com>
2024-05-14 15:37:44 -0500

    Bug fixes and Improvements

---
+++ profanityfilterQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class profanityfilterQueryOptions {
+public string text { get; set; }
+public string mask { get; set; }
+}
+
+}
